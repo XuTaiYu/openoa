d36d090f4045344553fdc98efdac69e68a418dc2
XuTaiYu <519346740@qq.com>
2019-07-21 03:40:15 +0800

    'first-commit'

---
+++ openoa/src/main/java/com/xxy/croe/OpenoaApplication.java
@@ -0,0 +1,13 @@
+package com.xxy.croe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenoaApplication.class, args);
+	}
+
+}
